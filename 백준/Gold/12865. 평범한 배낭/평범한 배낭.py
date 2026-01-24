e073efbfa88baebc766ab3ff162e7b5b18663fe8
huiseong29 <tobestar29@naver.com>
2026-01-24 09:59:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 2432 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,10 @@
+N, K = map(int, input().split())
+items = [tuple(map(int, input().split())) for _ in range(N)]
+
+dp = [0] * (K + 1)
+
+for w_i, v_i in items:
+    for w in range(K, w_i - 1, -1):
+        dp[w] = max(dp[w], dp[w - w_i] + v_i)
+
+print(dp[K])
